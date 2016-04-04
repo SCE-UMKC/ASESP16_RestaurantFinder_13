e2f94a7771a675a5f8e454c39ef2f04085638993
venkatagovardhan <venkatagovardhangoli@gmail.com>
2016-04-03 22:44:57 -0500

    type

---
+++ source code/319/Restaurantfinder/app/src/main/java/com/appdevelopment/venkatagovardhan/restaurantfinder/PlacesList.java
@@ -0,0 +1,8 @@
+package com.appdevelopment.venkatagovardhan.restaurantfinder;
+
+/**
+ * Created by Venkata Govardhan on 4/1/2016.
+ */
+public class PlacesList {
+    public Restaurant[] results;
+}
---
+++ source code/319/Restaurantfinder/app/src/main/java/com/appdevelopment/venkatagovardhan/restaurantfinder/Restaurant.java
@@ -0,0 +1,10 @@
+package com.appdevelopment.venkatagovardhan.restaurantfinder;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+/**
+ * Created by Venkata Govardhan on 4/1/2016.
+ */
+public class Restaurant {
+    public Barcode.CalendarEvent geometry;
+}
